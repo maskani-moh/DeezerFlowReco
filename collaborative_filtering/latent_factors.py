feb2b3c40f9055ee871b656919fd55c61e480423
Anthony Hu <anth.hu@gmail.com>
2017-05-01 22:31:36 +0100

    Latent factors submission

--- collaborative_filtering/latent_factors.py
+++ collaborative_filtering/latent_factors.py
@@ -16,59 +16,61 @@ def convert_rdd(df, sc):
     df_rdd = df_rdd.map(lambda r: Rating(int(r[0]), int(r[1]), float(r[2])))
     return df_rdd
 
-# Create a mask using 'listen_type'
-def create_mask(df, predictions):
-    pairs_user_song = predictions.map(lambda r: (r[0][0], r[0][1])).collect()
 
-    mask = []
-    for (user, song) in pairs_user_song:
-        mask.append(df[(df['user_id'] == user) & (df['media_id'] == song)]['listen_type'].values[0])
-    mask = np.array(mask)
-
-    # Convert 0-1 to boolean
-    mask = (mask == 1)
-    return mask
-
-# ROC AUC metric
-def compute_roc_auc(data, predictions, mask):
+def compute_y_pred(test_f, predictions, user_dict):
     """
     ROC AUC with rdd data
     """
+    ALS_pred = np.array(predictions.map(lambda r: [r[0][0], r[0][1], r[1]]).collect())
+
+    ALS_pred_df = pd.DataFrame({'user_id': ALS_pred[:, 0],
+                                'media_id': ALS_pred[:, 1],
+                                'is_listened_pred': ALS_pred[:, 2]})
+    ALS_pred_df['user_id'] = ALS_pred_df['user_id'].astype(int)
+    ALS_pred_df['media_id'] = ALS_pred_df['media_id'].astype(int)
+
+    test_f_merged = test_f.merge(ALS_pred_df, how='left', on=['user_id', 'media_id'])
     
-    # Join the predictions to the original data
-    data_pred = data.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-    y_true = np.array(data_pred.map(lambda r: (r[1][0])).collect())
-    y_pred = np.array(data_pred.map(lambda r: (r[1][1])).collect())
+    # Compute missing values with user avg
+    test_f_merged.loc[np.isnan(test_f_merged['is_listened_pred']), 'is_listened_pred'] = \
+        test_f_merged.loc[np.isnan(test_f_merged['is_listened_pred']), 'user_id'].map(user_dict)
+    
+    # Join the predictions to the original data. Delete instances in data that 
+    # does not match the ones in predictions
+    #data_pred = data.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+    #y_true = np.array(data_pred.map(lambda r: (r[1][0])).collect())
+    #y_pred = np.array(data_pred.map(lambda r: (r[1][1])).collect())
+    y_true = test_f_merged['is_listened']
+    y_pred = test_f_merged['is_listened_pred']
     y_pred = (y_pred - min(y_pred)) / (max(y_pred) - min(y_pred))
-    
-    roc_auc = roc_auc_score(y_true[mask], y_pred[mask])
-    return roc_auc
+
+    return (y_true, y_pred)
 
 if __name__ == '__main__':
 
-	full_train = pd.read_csv('../train.csv')
-	# Reduced train dataset
-	train = full_train[:1000000]
+	rank = 8
+	lmbda = 0.1
+	iterations = 10
 
-	# Initialise Spark
-	conf = SparkConf() \
-	      .set("spark.executor.memory", "2g")
 	sc = SparkContext(conf=conf)
 
-	rank_grid = [5, 10]
-	lmbda_grid = [0.1, 0.2]
-	nb_iterations = 10
+	# Need a column 'is_listened' in convert_rdd
+	test['is_listened'] = 0
+	train_rdd = convert_rdd(train, sc)
+	test_rdd = convert_rdd(test, sc)
 
-	# Need to compute the train/test split
+	model = ALS.train(train_rdd, rank=rank, 
+	                  iterations=iterations, lambda_=lmbda,
+	                  seed=random_seed)
 
-	for (rank, lmbda) in itertools.product(rank_grid, lmbda_grid):
-	    model = ALS.train(train_rdd, rank=rank, 
-	                      iterations=nb_iterations, lambda_=lmbda)
-	    
-	    # Test data without its last element for predictAll
-	    test_1_rdd_ = test_1_rdd.map(lambda r: (r[0], r[1]))
-	    predictions = model.predictAll(test_1_rdd_).map(lambda r: ((r[0], r[1]), r[2]))
-	    mask = create_mask(test_cv_1, predictions)
-	    
-	    print('Parameters: rank={0}, lambda={1}:'.format(rank, lmbda))
-	    print(compute_roc_auc(test_1_rdd, predictions, mask))
+	# Test data without its last element for predictAll
+	test_rdd_X = test_rdd.map(lambda r: (r[0], r[1]))
+	predictions = model.predictAll(test_rdd_X).map(lambda r: ((r[0], r[1]), r[2]))
+	user_dict = train.groupby('user_id')['is_listened'].mean().to_dict()
+	        
+	# Prediction
+	y_true, y_pred = compute_y_pred(test, predictions, user_dict)
+	test['is_listened'] = y_pred
+
+	test[['sample_id', 'is_listened']].to_csv('ALS_rank_8_lambda_01.csv',
+                                          index=False)
